699155c7656ac29addf8eda8d7b1c26c32eeba1a
MichalMatu <michal.matuszewski1984@gmail.com>
2023-12-17 11:10:09 +0000

    sender

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,38 @@ sensors_event_t humidity, temp;
 // create variable to store mac adress
 String mac;
 // REPLACE WITH RECEIVER MAC Address - 48:3F:DA:AA:0E:B9
+// sender mac address: 80:7D:3A:48:CD:FD
 uint8_t broadcastAddress[] = {0x48, 0x3F, 0xDA, 0xAA, 0x0E, 0xB9};
+// Structure example to send data
+// Must match the receiver structure
+typedef struct struct_message
+{
+  char a[32];
+  int b;
+  float c;
+  String d;
+  bool e;
+} struct_message;
+
+// Create a struct_message called myData
+struct_message myData;
+
+unsigned long lastTime = 0;
+unsigned long timerDelay = 2000; // send readings timer
+
+// Callback when data is sent
+void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus)
+{
+  Serial.print("Last Packet Send Status: ");
+  if (sendStatus == 0)
+  {
+    Serial.println("Delivery success");
+  }
+  else
+  {
+    Serial.println("Delivery fail");
+  }
+}
 
 void setup()
 {
@@ -41,16 +72,33 @@ void setup()
   Serial.print("ESP8266 Board MAC Address:  ");
   Serial.println(WiFi.macAddress());
   mac = WiFi.macAddress();
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+
+  // Init ESP-NOW
+  if (esp_now_init() != 0)
+  {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  // Once ESPNow is successfully Init, we will register for Send CB to
+  // get the status of Trasnmitted packet
+  esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
+  esp_now_register_send_cb(OnDataSent);
+
+  // Register peer
+  esp_now_add_peer(broadcastAddress, ESP_NOW_ROLE_SLAVE, 1, NULL, 0);
 }
 
 // update temp and humidity every 5 seconds
 // create variable to keep track of time
-unsigned long lastTime = 0;
-unsigned long timerDelay = 5000;
+unsigned long sensorLastTime = 0;
+unsigned long sensorTimerDelay = 5000;
 
 void loop()
 {
-  if (millis() - lastTime > timerDelay)
+  if (millis() - sensorLastTime > sensorTimerDelay)
   {
     aht.getEvent(&humidity, &temp); // populate temp and humidity objects with fresh data
     Serial.println(temp.temperature);
@@ -72,6 +120,21 @@ void loop()
     display.setCursor(10, 10);
     display.print(mac);
     display.display();
+    sensorLastTime = millis();
+  }
+
+  if ((millis() - lastTime) > timerDelay)
+  {
+    // Set values to send
+    strcpy(myData.a, "THIS IS A CHAR");
+    myData.b = random(1, 20);
+    myData.c = 1.2;
+    myData.d = "Hello";
+    myData.e = false;
+
+    // Send message via ESP-NOW
+    esp_now_send(broadcastAddress, (uint8_t *)&myData, sizeof(myData));
+
     lastTime = millis();
   }
 }
