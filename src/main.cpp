65448e7b127a3ebe979f9db6752ba7b9afa305f9
MichalMatu <michal.matuszewski1984@gmail.com>
2023-12-17 11:13:52 +0000

    reciver

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,12 @@
-#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
-#include <Adafruit_AHTX0.h>
+
+// REPLACE WITH RECEIVER MAC Address - 48:3F:DA:AA:0E:B9
+// sender mac address: 80:7D:3A:48:CD:FD
+
 #include <ESP8266WiFi.h>
 #include <espnow.h>
 
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
-
-// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-#define OLED_RESET -1 // Reset pin # (or -1 if sharing Arduino reset pin)
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-Adafruit_AHTX0 aht;
-sensors_event_t humidity, temp;
-
-// create variable to store mac adress
-String mac;
-// REPLACE WITH RECEIVER MAC Address - 48:3F:DA:AA:0E:B9
-// sender mac address: 80:7D:3A:48:CD:FD
-uint8_t broadcastAddress[] = {0x48, 0x3F, 0xDA, 0xAA, 0x0E, 0xB9};
-// Structure example to send data
-// Must match the receiver structure
+// Structure example to receive data
+// Must match the sender structure
 typedef struct struct_message
 {
   char a[32];
@@ -33,45 +19,30 @@ typedef struct struct_message
 // Create a struct_message called myData
 struct_message myData;
 
-unsigned long lastTime = 0;
-unsigned long timerDelay = 2000; // send readings timer
-
-// Callback when data is sent
-void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus)
+// Callback function that will be executed when data is received
+void OnDataRecv(uint8_t *mac, uint8_t *incomingData, uint8_t len)
 {
-  Serial.print("Last Packet Send Status: ");
-  if (sendStatus == 0)
-  {
-    Serial.println("Delivery success");
-  }
-  else
-  {
-    Serial.println("Delivery fail");
-  }
+  memcpy(&myData, incomingData, sizeof(myData));
+  Serial.print("Bytes received: ");
+  Serial.println(len);
+  Serial.print("Char: ");
+  Serial.println(myData.a);
+  Serial.print("Int: ");
+  Serial.println(myData.b);
+  Serial.print("Float: ");
+  Serial.println(myData.c);
+  Serial.print("String: ");
+  Serial.println(myData.d);
+  Serial.print("Bool: ");
+  Serial.println(myData.e);
+  Serial.println();
 }
 
 void setup()
 {
+  // Initialize Serial Monitor
   Serial.begin(115200);
-  Wire.begin(0, 2);
-  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
-  {
-    Serial.println(F("SSD1306 allocation failed"));
-    for (;;)
-      ; // Don't proceed, loop forever
-  }
-  if (!aht.begin())
-  {
-    Serial.println("Could not find AHT? Check wiring");
-    while (1)
-      delay(10);
-  }
-  Serial.println("AHT10 or AHT20 found");
-  Serial.begin(115200);
-  Serial.println();
-  Serial.print("ESP8266 Board MAC Address:  ");
-  Serial.println(WiFi.macAddress());
-  mac = WiFi.macAddress();
+
   // Set device as a Wi-Fi Station
   WiFi.mode(WIFI_STA);
 
@@ -82,59 +53,12 @@ void setup()
     return;
   }
 
-  // Once ESPNow is successfully Init, we will register for Send CB to
-  // get the status of Trasnmitted packet
-  esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
-  esp_now_register_send_cb(OnDataSent);
-
-  // Register peer
-  esp_now_add_peer(broadcastAddress, ESP_NOW_ROLE_SLAVE, 1, NULL, 0);
+  // Once ESPNow is successfully Init, we will register for recv CB to
+  // get recv packer info
+  esp_now_set_self_role(ESP_NOW_ROLE_SLAVE);
+  esp_now_register_recv_cb(OnDataRecv);
 }
-
-// update temp and humidity every 5 seconds
-// create variable to keep track of time
-unsigned long sensorLastTime = 0;
-unsigned long sensorTimerDelay = 5000;
 
 void loop()
 {
-  if (millis() - sensorLastTime > sensorTimerDelay)
-  {
-    aht.getEvent(&humidity, &temp); // populate temp and humidity objects with fresh data
-    Serial.println(temp.temperature);
-    Serial.println(humidity.relative_humidity);
-    // display text on screen
-    display.clearDisplay();
-    display.setTextSize(1);
-    display.setTextColor(WHITE);
-    display.setCursor(0, 0);
-    display.print("T: ");
-    display.setCursor(20, 0);
-    display.print(static_cast<int>(temp.temperature));
-    display.print("C");
-    display.setCursor(60, 0);
-    display.print("H: ");
-    display.setCursor(80, 0);
-    display.print(static_cast<int>(humidity.relative_humidity));
-    display.print("%");
-    display.setCursor(10, 10);
-    display.print(mac);
-    display.display();
-    sensorLastTime = millis();
-  }
-
-  if ((millis() - lastTime) > timerDelay)
-  {
-    // Set values to send
-    strcpy(myData.a, "THIS IS A CHAR");
-    myData.b = random(1, 20);
-    myData.c = 1.2;
-    myData.d = "Hello";
-    myData.e = false;
-
-    // Send message via ESP-NOW
-    esp_now_send(broadcastAddress, (uint8_t *)&myData, sizeof(myData));
-
-    lastTime = millis();
-  }
 }
