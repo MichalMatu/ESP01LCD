86e030598bf6a64c1005026fdcc9cb8ea465f0a2
MichalMatu <michal.matuszewski1984@gmail.com>
2023-12-17 06:53:46 +0000

    a

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,9 @@
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
+const int SensorPin = A0;
+int soilMoistureValue = 0;
+
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 #define OLED_RESET -1 // Reset pin # (or -1 if sharing Arduino reset pin)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
@@ -14,7 +17,7 @@ sensors_event_t humidity, temp;
 void setup()
 {
   Serial.begin(115200);
-  Wire.begin(2, 0);
+  Wire.begin(1, 3);
   if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
   {
     Serial.println(F("SSD1306 allocation failed"));
@@ -28,6 +31,8 @@ void setup()
       delay(10);
   }
   Serial.println("AHT10 or AHT20 found");
+  // set pin 2 as analog input
+  // pinMode(SensorPin, INPUT);
 }
 
 void loop()
@@ -47,6 +52,13 @@ void loop()
   display.print("Hum: ");
   display.setCursor(50, 20);
   display.println(humidity.relative_humidity);
+
+  // display soil moisture from sensor
+  soilMoistureValue = analogRead(SensorPin);
+  display.setCursor(0, 40);
+  display.print("Moisture: ");
+  display.setCursor(60, 40);
+  display.print(soilMoistureValue);
 
   display.display();
   delay(2000);
