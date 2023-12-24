d0debadcce3eefaf632da9f7439b72f8d2337eb1
MichalMatu <michal.matuszewski1984@gmail.com>
2023-12-24 18:47:45 +0000

    a

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,15 @@
-
 #include <ESP8266WiFi.h>
 #include <espnow.h>
 #include <DHT.h>
 #include <DHT_U.h>
+#include <Wire.h>
 
 #define DHTPIN 2
 #define DHTTYPE DHT22
 DHT dht(DHTPIN, DHTTYPE);
 
 #define uS_TO_S_FACTOR 1000000 /* Conversion factor for micro seconds to seconds */
-#define TIME_TO_SLEEP 5
+#define TIME_TO_SLEEP 60
 
 char mac[18]; // Change to char array
 
@@ -29,34 +29,35 @@ float h = 0.0;
 
 void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus)
 {
-  Serial.print("Last Packet Send Status: ");
-  if (sendStatus == 0)
-  {
-    Serial.println("Delivery success");
-    deviceConnected = true;
-  }
-  else
-  {
-    Serial.println("Delivery fail");
-    deviceConnected = false;
-  }
+  // Serial.print("Last Packet Send Status: ");
+  // if (sendStatus == 0)
+  // {
+  //   Serial.println("Delivery success");
+  //   deviceConnected = true;
+  // }
+  // else
+  // {
+  //   Serial.println("Delivery fail");
+  //   deviceConnected = false;
+  // }
 }
 
 void setup()
 {
-  Serial.begin(115200);
   dht.begin();
 
   // Use char array instead of String
   WiFi.macAddress().toCharArray(mac, 18);
-  Serial.print("ESP8266 Board MAC Address:  ");
-  Serial.println(mac);
+  // Serial.print("ESP8266 Board MAC Address:  ");
+  // Serial.println(mac);
+
+  Wire.begin(); // Initialize the I2C library
 
   WiFi.mode(WIFI_STA);
 
   if (esp_now_init() != 0)
   {
-    Serial.println("Error initializing ESP-NOW");
+    // Serial.println("Error initializing ESP-NOW");
     return;
   }
 
@@ -66,22 +67,18 @@ void setup()
   esp_now_add_peer(broadcastAddress, ESP_NOW_ROLE_SLAVE, 1, NULL, 0);
 }
 
-unsigned long sensorLastTime = 0;
-unsigned long sensorTimerDelay = 5000;
-
 void loop()
 {
-  if (millis() - sensorLastTime > sensorTimerDelay)
-  {
-    t = dht.readTemperature();
-    h = dht.readHumidity();
+  t = dht.readTemperature();
+  h = dht.readHumidity();
 
-    strcpy(myData.a, "THIS IS A SENSOR READINGS");
-    myData.b = t;
-    myData.c = h;
+  strcpy(myData.a, "DHT22");
+  myData.b = t;
+  myData.c = h;
 
-    esp_now_send(broadcastAddress, (uint8_t *)&myData, sizeof(myData));
+  esp_now_send(broadcastAddress, (uint8_t *)&myData, sizeof(myData));
 
-    sensorLastTime = millis();
-  }
+  // Serial.println("Going to sleep now...");
+  ESP.deepSleep(TIME_TO_SLEEP * uS_TO_S_FACTOR, RF_CAL);
+  delay(100); // Some delay to allow ESP8266 to enter deep sleep properly
 }
