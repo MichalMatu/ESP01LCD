c90e7d1813472f4aab9e15a543c96969014d4269
MichalMatu <michal.matuszewski1984@gmail.com>
2023-12-24 16:42:42 +0000

    dht22

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,13 @@
-#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
-#include <Adafruit_AHTX0.h>
+
 #include <ESP8266WiFi.h>
 #include <espnow.h>
-#include <Adafruit_ADS1X15.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
 
-#define SCREEN_WIDTH 128
-#define SCREEN_HEIGHT 64
-#define OLED_RESET -1
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-Adafruit_AHTX0 aht;
-sensors_event_t humidity, temp;
-
-Adafruit_ADS1115 ads;
+#define DHTPIN 2
+#define DHTTYPE DHT22
+DHT dht(DHTPIN, DHTTYPE);
 
 char mac[18]; // Change to char array
 
@@ -27,6 +21,9 @@ typedef struct struct_message
 struct_message myData;
 
 bool deviceConnected = false;
+
+float t = 0.0;
+float h = 0.0;
 
 void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus)
 {
@@ -46,20 +43,7 @@ void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus)
 void setup()
 {
   Serial.begin(115200);
-  Wire.begin(0, 2);
-  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
-  {
-    Serial.println(F("SSD1306 allocation failed"));
-    while (1)
-      ; // Don't proceed, loop forever
-  }
-  if (!aht.begin())
-  {
-    Serial.println("Could not find AHT? Check wiring");
-    while (1)
-      delay(10);
-  }
-  Serial.println("AHT10 or AHT20 found");
+  dht.begin();
 
   // Use char array instead of String
   WiFi.macAddress().toCharArray(mac, 18);
@@ -72,13 +56,6 @@ void setup()
   {
     Serial.println("Error initializing ESP-NOW");
     return;
-  }
-
-  if (!ads.begin())
-  {
-    Serial.println("Failed to initialize ADS.");
-    while (1)
-      ;
   }
 
   esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
@@ -97,107 +74,14 @@ void loop()
 {
   if (millis() - sensorLastTime > sensorTimerDelay)
   {
-    aht.getEvent(&humidity, &temp);
-
-    int16_t adc0, adc1, adc2, adc3;
-    float volts0, volts1, volts2, volts3;
-
-    adc0 = ads.readADC_SingleEnded(0);
-    adc1 = ads.readADC_SingleEnded(1);
-    adc2 = ads.readADC_SingleEnded(2);
-    adc3 = ads.readADC_SingleEnded(3);
-
-    volts0 = ads.computeVolts(adc0);
-    volts1 = ads.computeVolts(adc1);
-    volts2 = ads.computeVolts(adc2);
-    volts3 = ads.computeVolts(adc3);
-
-    // when volts0 is 1 then soil moisture is 100%, when volts0 is 2.15 then soil moisture is 0%. Make smooth transition between 1 and 2.15 to get soil moisture value
-    soilMoistureValue = map(adc0, airValue, waterValue, 0, 100);
-
-    Serial.println(temp.temperature);
-    Serial.println(humidity.relative_humidity);
-
-    display.clearDisplay();
-    display.setTextSize(1);
-    display.setTextColor(WHITE);
-    display.setCursor(0, 0);
-    display.print("T: ");
-    display.setCursor(20, 0);
-    display.print(static_cast<int>(temp.temperature));
-    display.print("C");
-    display.setCursor(60, 0);
-    display.print("H: ");
-    display.setCursor(80, 0);
-    display.print(static_cast<int>(humidity.relative_humidity));
-    display.print("%");
-    display.setCursor(0, 10);
-    display.print(mac);
-    // display adc0m adc1, adc2, adc3 on the screen
-    display.setCursor(0, 20);
-    display.print("AIN0: ");
-    display.setCursor(40, 20);
-    display.print(volts0);
-    display.print("V");
-    display.setCursor(0, 30);
-    display.print("AIN1: ");
-    display.setCursor(40, 30);
-    display.print(volts1);
-    display.print("V");
-    display.setCursor(0, 40);
-    display.print("AIN2: ");
-    display.setCursor(40, 40);
-    display.print(volts2);
-    display.print("V");
-    display.setCursor(0, 50);
-    display.print("AIN3: ");
-    display.setCursor(40, 50);
-    display.print(volts3);
-    display.print("V");
-
-    display.setCursor(80, 20);
-    display.print("SM: ");
-    display.setCursor(100, 20);
-    display.print(soilMoistureValue);
-
-    display.setCursor(115, 10);
-    if (deviceConnected)
-    {
-      display.print("1");
-    }
-    else
-    {
-      display.print("0");
-    }
-    display.display();
+    t = dht.readTemperature();
+    h = dht.readHumidity();
 
     strcpy(myData.a, "THIS IS A SENSOR READINGS");
-    myData.b = temp.temperature;
-    myData.c = humidity.relative_humidity;
+    myData.b = t;
+    myData.c = h;
 
     esp_now_send(broadcastAddress, (uint8_t *)&myData, sizeof(myData));
-
-    Serial.println("-----------------------------------------------------------");
-    Serial.print("AIN0: ");
-    Serial.print(adc0);
-    Serial.print("  ");
-    Serial.print(volts0);
-    Serial.println("V");
-    Serial.print("AIN1: ");
-    Serial.print(adc1);
-    Serial.print("  ");
-    Serial.print(volts1);
-    Serial.println("V");
-    Serial.print("AIN2: ");
-    Serial.print(adc2);
-    Serial.print("  ");
-    Serial.print(volts2);
-    Serial.println("V");
-    Serial.print("AIN3: ");
-    Serial.print(adc3);
-    Serial.print("  ");
-    Serial.print(volts3);
-    Serial.println("V");
 
     sensorLastTime = millis();
   }
